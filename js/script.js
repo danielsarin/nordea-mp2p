ae68f6406b4550924ad1a0c20378b092a333d742
Jasper <github@jasperheeffer.nl>
2014-05-09 01:33:48 +0300

    Updates:  - transactions button  - modal backdrop in registration cancel-alerts  - menu in all screens  - demo-popup also allows to continue demo  - account balance outside send/request/divide  - bank logo at account balance  - confirmation screen a bit more tidy (needs more)  - added 'go to transactions' at receipt-page.

--- js/script.js
+++ js/script.js
@@ -8,4 +8,18 @@ $(function() {
 	$('.cancel-continue').click(function() {
 		$('.cl-alert').hide();
 	});
+	
+	// side-menu
+    $(document).on('click', '.menu-button', function() {
+      if ($('.cl-page').position().left == 0) {
+        $('.side-menu').animate({ 'left': '+=250px' }, 400);
+        $('.cl-page').animate({ 'left': '+=250px' }, 400);
+        $('.cl-page').one('click', function() {
+          $('.side-menu').animate({ 'left': '-=250px' }, 400);
+          $('.cl-page').animate({ 'left': '-=250px' }, 400);
+        });
+      } 
+    });
+	
 });
+
